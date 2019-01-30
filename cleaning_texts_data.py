815736a919e14aedf0a9b216eaa7421288286a11
Desire Iradukunda <35916017+Desire100@users.noreply.github.com>
2019-01-30 19:26:22 +0800

    Update cleaning_texts_data.py

--- cleaning_texts_data.py
+++ cleaning_texts_data.py
@@ -1,9 +1,7 @@
 # cleanig cornell movie corpus
 # Importing the libraries
-import numpy as np
-import tensorflow as tf
+
 import re
-import time
 
 # Importing the dataset
 lines = open('movie_lines.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
